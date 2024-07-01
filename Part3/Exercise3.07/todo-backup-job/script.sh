5ebd7479518027cf939462cc439d6fbdf976c8a4
sushashi <shuishan.ly@gmail.com>
2024-07-01 09:38:11 +0200

    Ex3.07 first

---
+++ Part3/Exercise3.07/todo-backup-job/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+
+URL="postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}"
+
+pg_dump -v $URL > backup.sql
+
+echo "Backing up database, $TIMESTAMP"
+
+curl -X POST --upload-file backup.txt \
+    -H "Authorization: Bearer ${TOKEN}" \
+    "https://storage.googleapis.com/upload/storage/v1/b/dwk-project-backup-bucket/o?uploadType=media&name=backup-${TIMESTAMP}.txt"

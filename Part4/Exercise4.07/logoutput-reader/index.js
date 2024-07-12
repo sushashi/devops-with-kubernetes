57973047eba39505fc2deeabbfeaeb9dfd8e5933
sushashi <shuishan.ly@gmail.com>
2024-07-12 14:19:02 +0200

    asd

--- Part4/Exercise4.07/logoutput-reader/index.js
+++ Part4/Exercise4.07/logoutput-reader/index.js
@@ -67,7 +67,7 @@ app.get("/", async (req, res) => {
     fileConfigText = await getConfigText()
 
     res.set('Content-Type', 'text/html')
-    res.send('FILE cbv: '+ fileConfigText + '<br>' + 'env variable: MESSAGE=' + MSG + '<br>' + theString + '.<br>' + "Ping / Pongs: " + counterPong)
+    res.send('FILE nbv: '+ fileConfigText + '<br>' + 'env variable: MESSAGE=' + MSG + '<br>' + theString + '.<br>' + "Ping / Pongs: " + counterPong)
 })
 
 app.get("/readerz", checkDb )
